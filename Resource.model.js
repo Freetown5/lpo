040aca78f82f428fd99ce55c52369e1531c0a4a9
Isa <iconteh21@gmail.com>
2017-01-03 12:46:26 -0500

    instantiating stuff

--- Resource.model.js
+++ Resource.model.js
@@ -5,7 +5,33 @@ var ResourceSchema = new Schema({
 	title: String,
 	url: String,
 	languages: Array,
-	cost: Number,
+	costMin: Number,
+	costMax: Number,
 })
 
 module.exports = mongoose.model('Resource', ResourceSchema);
+
+"Bloc.io":{
+	"url":"https://www.bloc.io/",
+	"languages":[
+		"HTML/CSS",
+		"CLI",
+		"Git",
+		"Javascript",
+		"Ruby",
+		],
+	"costMin":"0",
+	"costMax":"19500",
+	}
+
+// db.resourceSchema.insert({
+// 	title: 'Bloc.io',
+// 	url: 'https://www.bloc.io/',
+// 	languages: ['HTML/CSS', 'CLI', 'Git', 'Javascript', 'Ruby'],
+// 	costMin: 0,
+// 	costMax: 19500
+// })
+
+
+
+

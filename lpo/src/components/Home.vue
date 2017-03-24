65bfbef19ea606c7e72799130319c02f5b6a2fc5
Isa <iconteh21@gmail.com>
2017-03-24 16:55:40 -0400

    added content for slider intro, also started setting up testing for home component

--- lpo/src/components/Home.vue
+++ lpo/src/components/Home.vue
@@ -12,25 +12,38 @@
       <!-- sliding content -->
       <md-layout md-flex-xsmall="100" md-flex-small="50" md-flex-medium="50">
         <div class="home_container">
-          <md-avatar class="md-avatar-icon md-accent">
+          <!-- <md-avatar class="md-avatar-icon md-accent">
             <md-icon>exit_to_app</md-icon>
-          </md-avatar>
-          <p>sliding content here</p>
+          </md-avatar> -->
+          <i class="material-icons">keyboard_arrow_left</i>
+          <p class="home_intro">{{ content[2] }}</p>
+          <i class="material-icons">keyboard_arrow_right</i>
         </div>
       </md-layout>
     </md-layout>
 
+  <!-- Icons are not extended so they have their original class names-->
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Home'
+    name: 'Home',
+    data () {
+      return {
+        content: [ 'Welcome to LearnProg. Your source for finding great on line learning tools for programming.',
+          'First, select which option is most important to you when choosing a programming resource.',
+          'Then, continue narrowing down the sources you want by entering more information about what you are looking for before clicking "filter".',
+          'After you\'ve gotten the results you wanted, find out more about each of the choices listed by clicking on the learn more button next to it.',
+          'Have fun learning!!' ]
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
 /* tried setting padding and margin of all outer containers to 0, didn't work */
+
   $middle-grey:#A2A2A0;
 
   .home{
@@ -42,6 +55,19 @@
     &_container{
       margin-top: 33%;
       margin-left: 10%;
+      width: 100%
+    }
+
+    &_intro{
+      width:70%;
+      float:left;
     }
   }
+
+  .material-icons{
+    float: left;
+    margin: 2%;
+    margin-top: 5%;
+  }
+
 </style>

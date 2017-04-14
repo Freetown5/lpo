f8c36b45af2aa981017652153742cafd4edce3ff
Isa <iconteh21@gmail.com>
2017-04-14 13:54:11 -0400

    got method to workgit status

--- lpo/src/components/Home.vue
+++ lpo/src/components/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="home">
      <!-- <h1>{{ header }}</h1> -->
-    <h1>{{ header[1] }}</h1>
+    <h1>{{ header }}</h1>
     <md-avatar class="md-avatar-icon md-accent">
       <md-icon>exit_to_app</md-icon>
     </md-avatar>
-    <button>Get something in console</button>
+    <!-- The below equation in v-on worked when h1 was set to {{ header }} -->
+    <!-- <button v-on:click="header = header[1]">Get something in console</button> -->
+    <button v-on:click="consoleWorking()">Get something in console</button>
     <ul>
       <li>Words</li>
     </ul> 
@@ -24,11 +26,9 @@
         ]
       }
     },
-    methods: function () {
-      return {
-        consoleWorking: function () {
-          console.log('console working?')
-        }
+    methods: {
+      consoleWorking () {
+        console.log('console working?')
       }
     }
   }

9b89c7da9f9a9526c18964f9b4f3681661e82d00
Isa Conteh <iconteh21@gmail.com>
2018-10-09 20:04:46 -0400

    finally figured out how to isolate firebase data to home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,14 +9,36 @@
     <!-- <button v-on:click="header = header[1]">Get something in console</button> -->
     <button v-on:click="consoleWorking()">Get something in console</button>
     <ul>
-      <li>Words</li>
-    </ul> 
+      <li class="space" v-for="(value, key) in source[0]">
+      {{ key }}
+        <br>
+        {{ value.url }}  
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import Firebase from 'firebase'
+
+  let config = {
+    apiKey: 'AIzaSyCHs1Kln9hhK9Ua5-b48KPRHWcAj6bMN8U',
+    authDomain: 'progsources-2aa3d.firebaseapp.com',
+    databaseURL: 'https://progsources-2aa3d.firebaseio.com',
+    storageBucket: 'progsources-2aa3d.appspot.com',
+    messagingSenderId: '456563027788'
+  }
+
+  let app = Firebase.initializeApp(config)
+  let db = app.database()
+
+  let sourceRef = db.ref('learning-type')
+
   export default {
     name: 'home',
+    firebase: {
+      source: sourceRef
+    },
     data: function () {
       return {
         header: [

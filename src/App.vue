844ead67e37a4b4c57d04dc072aa68b4830404e5
Isa Conteh <iconteh21@gmail.com>
2018-10-18 14:33:18 -0400

    updated node-sass and lib-sass

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,25 @@
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="scss">
+  // Color Scheme
+  $pale-green: #b9c6ae;
+  $navy: #0b4f6c;
+  $rust: #875f62; 
+  $moss-green: #7a9b76;
+  $blue-green: #0892a5;
+
+  .md-theme-default{
+    background: $pale-green;
+  }
+
+  #app { 
+    font-family: 'Avenir', Helvetica, Arial, sans-serif; 
     -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    /*text-align: center;*/
-    color: #2c3e50;
-    margin-top: 60px;
+    -moz-osx-font-smoothing: grayscale; 
+    /*text-align: center;*/ 
+    color: #2c3e50; 
+    margin-top: 60px;    
     margin:10px;
   }
 </style>
